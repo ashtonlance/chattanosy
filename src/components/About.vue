c403846781524dd545c5d9e451802a9f32b4f767
Ashton Lance <ashton@ashtonlance.com>
2017-05-19 20:04:24 -0400

    Added todo list

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,6 +4,13 @@
       <h1>Hello, this is Chattanosy</h1>
       <p>If you're like me, you can't help but wonder what that new construction project is that going on down the street. This is a place for the community to come together and share their knowledge and insights about their surroundings. Feel free to add your information to the map!</p>
       <p>This site is very much in a beta state - but please send me any feedback you may have <a href="mailto:ashton@ashtonlance.com">here.</a></p>
+      <ul>
+        <h1>Todo List:</h1>
+        <li>Add ability for users to reply to orginal post</li>
+        <li>Add ability to edit posts (by the orginal user only)</li>
+        <li>Add ability to use current location (good for mobile)</li>
+        <li>Add logo/branding</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -41,14 +48,14 @@ h2 {
 }
 
 ul {
-  list-style-type: none;
+  list-style-type: none !important;
   padding: 0;
 }
 
-li {
+/*li {
   display: inline-block;
   margin: 0 10px;
-}
+}*/
 
 a {
   color: #42b983;
